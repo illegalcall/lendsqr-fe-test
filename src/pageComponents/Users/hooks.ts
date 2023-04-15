350ba8fa466611eeb6d7d4a6e8d20edb8e8f6a72
dhruv <dhruv6687@gmail.com>
2023-04-15 16:17:18 +0530

    added users data to indexeddb

--- src/pageComponents/Users/hooks.ts
+++ src/pageComponents/Users/hooks.ts
@@ -1,26 +1,75 @@
+import { IUsersDetail } from '@/shared/types';
 import axios from 'axios';
 import { useState } from 'react';
 import { useQuery, useQueryClient } from 'react-query';
+
+import { openDB } from 'idb';
+
+const CACHE_INVALIDATION_TIME = 1000 * 60; // 1 min
 
 export const useUsers = () => {
   const [users, setUsers] = useState([]);
   const queryClient = useQueryClient();
 
-  const fetchUsers = async () => {
-    const data = await axios.get('http://localhost:3000/api/users', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
+  const initDb = async () => {
+    const db = await openDB('lendsqr-fe-test', 1, {
+      upgrade(db) {
+        db.createObjectStore('users', { keyPath: 'id' });
       },
     });
-    return data;
+    return db;
   };
+
+  const dbPromise = initDb();
+
+  async function saveUsers(users: IUsersDetail[]) {
+    const db = await dbPromise;
+    const tx = db.transaction('users', 'readwrite');
+    const store = tx.objectStore('users');
+    await store.clear();
+    await Promise.all(users.map((user) => store.put(user)));
+    await tx.done;
+    // set the current time in localstorage to be used to check if the cache is stale
+    localStorage.setItem('users-last-updated', new Date().getTime().toString());
+  }
+
+  async function loadUsers() {
+    const db = await dbPromise;
+    const tx = db.transaction('users');
+    const store = tx.objectStore('users');
+    const users = await store.getAll();
+    await tx.done;
+    return users;
+  }
+
+  const fetchUsers = async () => {
+    // check if the cache is stale
+    const lastUpdated = localStorage.getItem('users-last-updated');
+    //compare the current time with the last updated time and if the difference is greater than the cache invalidation time, fetch the data from the server
+    if (
+      lastUpdated &&
+      new Date().getTime() - parseInt(lastUpdated) > CACHE_INVALIDATION_TIME
+    ) {
+      console.log('hi');
+      const data = await axios.get('http://localhost:3000/api/users', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      return data;
+    }
+    // if the cache is not stale, load the data from the cache
+    const users = await loadUsers();
+    return { data: { users } };
+  };
+
   const { isLoading, data } = useQuery('users', fetchUsers, {
-    onSuccess: (data) => {
-      console.log(data);
+    onSuccess: async (data) => {
+      await saveUsers(data.data.users);
       setUsers(data.data.users);
     },
   });
-  console.log('🚀 ~ file: hooks.ts:23 ~ useUsers ~ isLoading:', isLoading);
+
   return {
     users,
     isLoading,
